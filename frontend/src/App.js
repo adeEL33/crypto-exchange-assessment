aacb7ff449cc1b16ccae5f647dcfc9937b8e3b8e
Adeel <adeel@xelent.io>
2023-02-24 14:15:19 +0500

    sorting added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,43 +2,57 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Form from "./Form";
 import Listing from "./Listing";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   //defining loading state
   const [loader, setLoader] = useState(false);
   const [exchanges, setExchanges] = useState([]);
-  const [sortType, setSortType] = useState('DESC');
-  const [sortColumn, setSortColumn] = useState('id');
-  const [date, setDate] = useState('');
-  const [type, setType] = useState('');
-
-
+  const [sortType, setSortType] = useState("DESC");
+  const [sortColumn, setSortColumn] = useState("id");
+  const [date, setDate] = useState("");
+  const [type, setType] = useState("");
 
   const fetchRecords = () => {
     handleLoading(true);
-    axios.get('http://localhost:8080/api/crypto_exchange_record?sort_type=' + sortType + '&sort_column=' + sortColumn + '&date=' + date + '&type=' + type).then((res) => {
-      if (res?.data?.status == 'success') {
-        setExchanges(res?.data?.data);
-      }
-    }).catch((err) => {
-      console.log(err);
-    }).finally(() => handleLoading(false));
-  }
+    axios
+      .get(
+        "http://localhost:8080/api/crypto_exchange_record?sort_type=" +
+          sortType +
+          "&sort_column=" +
+          sortColumn +
+          "&date=" +
+          date +
+          "&type=" +
+          type
+      )
+      .then((res) => {
+        if (res?.data?.status == "success") {
+          setExchanges(res?.data?.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => handleLoading(false));
+  };
   const setDateFilter = (dateObject) => {
     console.log(dateObject);
     setDate(dateformate(dateObject?.date));
-  }
+  };
   const setTypeFilter = (typeFilter) => {
     setType(typeFilter);
-  }
-  useEffect(() => {
-    fetchRecords();
-  }, [date, type])
+  };
+
   //set loader true or false
   const handleLoading = (isLoading) => {
     setLoader(isLoading);
-  }
+  };
+
+  const setSortTypeAndSortColumn = (sort_type, sort_olumn) => {
+    setSortColumn(sort_olumn);
+    setSortType(sort_type);
+  };
 
   const dateformate = (date) => {
     var year = date.getFullYear();
@@ -46,36 +60,42 @@ function App() {
     var day = date.getDate();
     var hour = date.getHours();
     var minutes = date.getMinutes();
-    if (month < 10)
-      month = "0" + month;
-    if (minutes < 10)
-      minutes = "0" + minutes;
-    if (day < 10)
-      day = "0" + day;
-    if (hour < 10)
-      hour = "0" + hour;
+    if (month < 10) month = "0" + month;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (day < 10) day = "0" + day;
+    if (hour < 10) hour = "0" + hour;
 
     return year + "-" + month + "-" + day;
-  }
+  };
 
+  useEffect(() => {
+    fetchRecords();
+  }, [date, type, sortColumn, sortType]);
   return (
-    <div >
-      {
-
-        loader ? (<div id="backdrop">
+    <div>
+      {loader ? (
+        <div id="backdrop">
           <div className="text-center loading">
             <div className="spinner-border" role="status">
               <span className="sr-only">Loading...</span>
             </div>
           </div>
-        </div>) : ''
-
-      }
+        </div>
+      ) : (
+        ""
+      )}
 
       <div className="container mt-5">
-
         <Form handleLoading={handleLoading} />
-        <Listing exchanges={exchanges} date={date} setDateFilter={setDateFilter} setTypeFilter={setTypeFilter} />
+        <Listing
+          setSortTypeAndSortColumn={setSortTypeAndSortColumn}
+          exchanges={exchanges}
+          date={date}
+          setDateFilter={setDateFilter}
+          setTypeFilter={setTypeFilter}
+          sortColumn={sortColumn}
+          sortType={sortType}
+        />
       </div>
       <ToastContainer />
     </div>
--- frontend/src/Listing.js
+++ frontend/src/Listing.js
@@ -1,80 +1,175 @@
 import "flatpickr/dist/themes/material_green.css";
 import Flatpickr from "react-flatpickr";
 
-function Listing({ exchanges, setDateFilter, setTypeFilter }) {
-
-    return (
-        <div className="card mt-5">
-            <div className="card-header">
-                <div className="row">
-                    <div className="col-lg-6 col-md-6 col-sm-12">
-                        <div className="form-group">
-                            <label>Date Filter</label>
-                            <Flatpickr
-                                className="form-control mt-2"
-                                onChange={
-                                    ([date]) => {
-                                        setDateFilter({ date });
-                                    }
-                                }
-                            />
-                        </div>
-                    </div>
-                    <div className="col-lg-6 col-md-6 col-sm-12">
-                        <div className="form-group">
-                            <label>Type Filter</label>
-                            <select onChange={
-                                (e) => setTypeFilter(e.target.value)
-                            } className="form-control mt-2">
-                                <option value="">select</option>
-                                <option value="live_price">Live Price</option>
-                                <option value="exchanged">Exchanged</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-
+function Listing({
+  exchanges,
+  setDateFilter,
+  setTypeFilter,
+  setSortTypeAndSortColumn,
+  sortType,
+  sortColumn,
+}) {
+  return (
+    <div className="card mt-5">
+      <div className="card-header">
+        <div className="row">
+          <div className="col-lg-6 col-md-6 col-sm-12">
+            <div className="form-group">
+              <label>Date Filter</label>
+              <Flatpickr
+                className="form-control mt-2"
+                onChange={([date]) => {
+                  setDateFilter({ date });
+                }}
+              />
             </div>
-            <div className="card-body">
-                <div class="table-responsive d-none d-sm-block">
-                    <table className="table table-striped table-responsive">
-                        <thead>
-                            <tr>
-                                <th>Date & Time</th>
-                                <td>Currency From</td>
-                                <td>Amount 1</td>
-                                <td>Currency To</td>
-                                <td>Amount 2</td>
-                                <td>Type</td>
-                            </tr>
-
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th colSpan={6}></th>
-                            </tr>
-
-                            {
-                                exchanges?.length > 0 && exchanges?.map((exchange, key) => {
-                                    return (
-                                        <tr key={key}>
-                                            <td>{exchange?.dated}</td>
-                                            <td>{exchange?.currency_from}</td>
-                                            <td>{exchange?.amount_1}</td>
-                                            <td>{exchange?.currency_to}</td>
-                                            <td>{exchange?.amount_2}</td>
-                                            <th className={exchange?.type == 'live_price' ? 'text text-success' : 'text text-primary'}>{exchange?.type == 'live_price' ? 'Live Price' : 'Exchanged'}</th>
-
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
-                </div>
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-12">
+            <div className="form-group">
+              <label>Type Filter</label>
+              <select
+                onChange={(e) => setTypeFilter(e.target.value)}
+                className="form-control mt-2"
+              >
+                <option value="">select</option>
+                <option value="live_price">Live Price</option>
+                <option value="exchanged">Exchanged</option>
+              </select>
             </div>
+          </div>
         </div>
-    );
+      </div>
+      <div className="card-body">
+        <div class="table-responsive d-none d-sm-block">
+          <table className="table table-striped table-responsive">
+            <thead>
+              <tr>
+                <th>
+                  <i
+                    className="fas fa-sort-amount-down"
+                    style={
+                      sortType == "ASC" && sortColumn == "dated"
+                        ? { color: "blue" }
+                        : {}
+                    }
+                    onClick={() => setSortTypeAndSortColumn("ASC", "dated")}
+                  ></i>
+                  <i
+                    className="fas fa-sort-amount-up"
+                    style={ sortType == 'DESC' && sortColumn == 'dated'? {color:'blue'}: {}}
+                    onClick={() => setSortTypeAndSortColumn("DESC", "dated")}
+                  ></i>
+                  Date & Time
+                </th>
+                <td>
+                  <i
+                    className="fas fa-sort-amount-down"
+                    style={ sortType == 'ASC' && sortColumn == 'currency_from'? {color:'blue'}: {}}
+                    onClick={() =>
+                      setSortTypeAndSortColumn("ASC", "currency_from")
+                    }
+                  ></i>{" "}
+                  <i
+                    className="fas fa-sort-amount-up"
+                    style={ sortType == 'DESC' && sortColumn == 'currency_from'? {color:'blue'}: {}}
+                    onClick={() =>
+                      setSortTypeAndSortColumn("DESC", "currency_from")
+                    }
+                  ></i>{" "}
+                  Currency From
+                </td>
+                <td>
+                  <i
+                    className="fas fa-sort-amount-down"
+                    style={ sortType == 'ASC' && sortColumn == 'amount_1'? {color:'blue'}: {}}
+                    onClick={() => setSortTypeAndSortColumn("ASC", "amount_1")}
+                  ></i>{" "}
+                  <i
+                    className="fas fa-sort-amount-up"
+                    style={ sortType == 'DESC' && sortColumn == 'amount_1'? {color:'blue'}: {}}
+                    onClick={() => setSortTypeAndSortColumn("DESC", "amount_1")}
+                  ></i>{" "}
+                  Amount 1
+                </td>
+                <td>
+                  <i
+                    className="fas fa-sort-amount-down"
+                    onClick={() =>
+                      setSortTypeAndSortColumn("ASC", "currency_to")
+                    }
+                    style={ sortType == 'ASC' && sortColumn == 'currency_to'? {color:'blue'}: {}}
+                  ></i>{" "}
+                  <i
+                    className="fas fa-sort-amount-up"
+                    onClick={() =>
+                      setSortTypeAndSortColumn("DESC", "currency_to")
+                    }
+                    style={ sortType == 'DESC' && sortColumn == 'currency_to'? {color:'blue'}: {}}
+                  ></i>{" "}
+                  Currency To
+                </td>
+                <td>
+                  <i
+                    className="fas fa-sort-amount-down"
+                    onClick={() => setSortTypeAndSortColumn("ASC", "amount_2")}
+                    style={ sortType == 'ASC' && sortColumn == 'amount_2'? {color:'blue'}: {}}
+                  ></i>{" "}
+                  <i
+                    className="fas fa-sort-amount-up"
+                    onClick={() => setSortTypeAndSortColumn("DESC", "amount_2")}
+                    style={ sortType == 'DESC' && sortColumn == 'amount_2'? {color:'blue'}: {}}
+                  ></i>{" "}
+                  Amount 2
+                </td>
+                <td>
+                  <i
+                    className="fas fa-sort-amount-down"
+                    onClick={() => setSortTypeAndSortColumn("ASC", "type")}
+                    style={ sortType == 'ASC' && sortColumn == 'type'? {color:'blue'}: {}}
+                  ></i>{" "}
+                  <i
+                    className="fas fa-sort-amount-up"
+                    onClick={() => setSortTypeAndSortColumn("DESC", "type")}
+                    style={ sortType == 'DESC' && sortColumn == 'type'? {color:'blue'}: {}}
+                  ></i>{" "}
+                  Type
+                </td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th colSpan={6}></th>
+              </tr>
+
+              {exchanges?.length > 0 &&
+                exchanges?.map((exchange, key) => {
+                  return (
+                    <tr key={key}>
+                      <td>{exchange?.dated}</td>
+                      <td>{exchange?.currency_from}</td>
+                      <td>{exchange?.amount_1}</td>
+                      <td>{exchange?.currency_to}</td>
+                      <td>{exchange?.amount_2}</td>
+                      <th
+                        className={
+                          exchange?.type == "live_price"
+                            ? "text text-success"
+                            : "text text-primary"
+                        }
+                      >
+                        {exchange?.type == "live_price"
+                          ? "Live Price"
+                          : "Exchanged"}
+                      </th>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Listing;
